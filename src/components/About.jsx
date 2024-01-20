9ffab29e63ed772e8708a9ba70b77dae01e8444b
Levy Monteiro <levymonteiro268@gmail.com>
2024-01-20 15:04:48 -0300

    update About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div
       id='about'
-      className='text-neutral-900 dark:text-neutral-50 font-nunito flex flex-col items-center gap-2 w-[85%] xs:w-[90%] md:w-[80%]'
+      className='text-neutral-900 dark:text-neutral-50 font-nunito flex flex-col items-center gap-3 w-[85%] xs:w-[90%] md:w-[80%]'
     >
       <img
         src='https://avatars.githubusercontent.com/u/102385102?v=4'
@@ -10,10 +10,8 @@ export default function About() {
         className='w-48 h-48 sm:w-52 sm:h-52 rounded-full border-[3px] border-primary p-[6px] hover:border-0 hover:scale-105 ease-in-out duration-200'
       />
       <h1 className='font-bold text-xl xs:text-2xl'>Levy Monteiro</h1>
-      <h2 className='font-bold opacity-90 text-[15px]'>About me</h2>
       <p className='opacity-80 text-[14px] xs:text-[15px] text-center'>
-        Hello my name is Levy Monteiro, i'm 22 years old, i live on Rio de
-        Janeiro, Brazil, and i'm a student of Web Development/Programming.
+        Front-end Web Developer based on Rio de Janeiro, Brazil.
       </p>
     </div>
   );
